ff63957c42c330e681a1bccaa996de62e4a709cc
n36165 <n36165@206-25>
2022-10-29 10:59:22 +0500

    Добавьте файлы проекта.

---
+++ PraktikaHeh/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PraktikaHeh
+{
+    /*delegate void FullName(string fullname);
+    class Class1
+    {
+        public int sum;
+        public string name;
+        FullName taken;
+        public void reg(FullName fullname)
+        {
+
+        }
+        public void A(int sum, ListBox listbox)
+        {
+            if (this.sum >= sum)
+            {
+                this.sum -= sum;
+                listbox.Items.Add($"Осталось:{ this.sum}");
+            }
+            else
+            {
+                listbox.Items.Add($"Мало денег");
+            }
+            
+        }
+
+        public void B(int sum, ListBox listBox)
+        {
+            this.sum += sum;
+            listBox.Items.Add($"Деньги: {this.sum}");
+            
+        }
+        public void FullName(int sum, string name)
+        {
+            this.sum = sum;
+            this.name = name;
+        }
+        public void Delete(FullName a)
+        {
+            taken -= a;
+        }
+    */
+    }
---
+++ PraktikaHeh/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary1;
+
+namespace PraktikaHeh
+{
+    public partial class Form1 : Form
+    {
+        FullName a;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = new FullName(Convert.ToInt32(textBox2.Text), textBox1.Text);
+            listBox1.Items.Add($"Имя:{a.name}");
+            listBox1.Items.Add($"Сумма:{a.sum}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a.Add(Convert.ToInt32(textBox3.Text), listBox1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FullName.B(Convert.ToInt32(textBox3.Text));
+        }
+    }
+}
